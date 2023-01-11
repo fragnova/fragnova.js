ca41168400d88adef103357395acbd5e26de00cf
aang114 <karandevsapra@gmail.com>
2023-01-11 13:50:40 +0800

    make the polkadot.js type definitions into a separate npm package

--- packages/types/src/interfaces/definitions.ts
+++ packages/types/src/interfaces/definitions.ts
@@ -1,3 +1,5 @@
+// Note from Karan: The generation scripts will load this file to determine which types it needs to import. Source: https://polkadot.js.org/docs/api/examples/promise/typegen/
+
 export { default as accounts } from '../../src/interfaces/accounts/definitions';
 export { default as detach } from '../../src/interfaces/detach/definitions';
 export { default as fragments } from '../../src/interfaces/fragments/definitions';
