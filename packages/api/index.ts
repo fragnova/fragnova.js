ef4506a4ea6b2d00ad2c39ae4e0839ac307c0f22
aang114 <karandevsapra@gmail.com>
2023-01-12 02:04:40 +0800

    added RPC functions to @fragcolor/api

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -3,11 +3,53 @@ import {definitions} from "@fragcolor/types";
 
 const types = Object.values(definitions).reduce((res, { types }): object => ({ ...res, ...types }), {});
 
-export async function createFragnovaApi(): Promise<ApiPromise> {
+export async function createFragnovaApi(rpcProvider): Promise<ApiPromise> {
     const api = await ApiPromise.create({
+        provider: rpcProvider,
         types: {
             ...types,
-        }
+        },
+        rpc: {
+            protos: {
+                getProtos: {
+                    description: "Query and Return Proto-Fragment(s) based on `params`. The return type is a JSON string", type: "String",
+                    params: [
+                        { name: "params", type: "GetProtosParams" },
+                        { name: "at", type: "BlockHash", isOptional: true }
+                    ]
+                },
+                getGenealogy: {
+                    description: "Query the Genealogy of a Proto-Fragment based on `params`. The return type is a JSON string that represents an Adjacency List.", type: "String",
+                    params: [
+                        { name: "params", type: "GetGenealogyParams" },
+                        { name: "at", type: "BlockHash", isOptional: true }
+                    ]
+                },
+            },
+            fragments: {
+                getDefinitions: {
+                    description: "Query and Return Fragment Definition(s) based on `params`", type: "String",
+                    params: [
+                        { name: "params", type: "GetDefinitionsParams" },
+                        { name: "at", type: "BlockHash", isOptional: true }
+                    ]
+                },
+                getInstances: {
+                    description: "Query and Return Fragment Instance(s) based on `params`", type: "String",
+                    params: [
+                        { name: "params", type: "GetInstancesParams" },
+                        { name: "at", type: "BlockHash", isOptional: true }
+                    ]
+                },
+                getInstanceOwner: {
+                    description: "Query the owner of a Fragment Instance. The return type is a String", type: "String",
+                    params: [
+                        { name: "params", type: "GetInstanceOwnerParams" },
+                        { name: "at", type: "BlockHash", isOptional: true }
+                    ]
+                },
+            },
+        },
     });
 
     return api;
